ccee118d54d93c547d8f9dfd789a07655ccd398a
Artur Jaakman <jaakmanartur@gmail.com>
2018-08-27 12:25:44 +0100

    Shaders.

--- GameEngineCore/main.cpp
+++ GameEngineCore/main.cpp
@@ -2,41 +2,48 @@
 
 #include "window.h"
 #include "src/maths/maths.h"
+#include "src/graphics/shader.h"
 
 int main()
 {
 	using namespace engine;
 	using namespace graphics;
 	using namespace maths;
+
 	Window window("Main Window ", 960, 540);
-	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
-
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	
-	mat4 position = mat4::translation(vec3(2, 3, 4));
-	position *= mat4::identity();
+	GLfloat vertices[] =
+	{
+		0, 0, 0,
+		8, 0, 0,
+		0, 3, 0,
+		0, 3, 0,
+		8, 3, 0,
+		8, 0, 0
+	};
+	
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
 
-	vec4 a(1.0f, 2.0f, 3.5f, 0.0f);
-	vec4 b(1.0f, 2.0f, 5.2f, 1.0f);
+	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
-	a /= b;
+	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
+	shader.enable();
+	shader.setUniformMat4("pr_matrix", ortho);
+	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
 
+	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+	shader.setUniform4f("colour", vec4(0.2f, 0.3f, 0.8f, 1.0f));
 
 	while (!window.closed())
 	{
 		window.clear();
-		std::cout << (a==b) << std::endl;
-
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f( 0.0f,  0.5f);
-		glVertex2f( 0.5f, -0.5f);
-		glEnd();
-		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
-
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		window.update();
 	}
 
