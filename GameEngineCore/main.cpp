c448f7955af56acaec8bce50fa055973138a5510
Artur Jaakman <jaakmanartur@gmail.com>
2018-09-01 12:11:47 +0100

    FreeImage.

--- GameEngineCore/main.cpp
+++ GameEngineCore/main.cpp
@@ -18,6 +18,7 @@
 
 #include "src/graphics/layers/tilelayer.h"
 
+#include "src/graphics/texture.h"
 #include "src/graphics/layers/group.h"
 
 #include <time.h>
@@ -25,6 +26,7 @@
 #define BATCH_RENDERER 1
 #define TEST_50K_SPRITES 0
 
+#if 1
 int main()
 {
 	using namespace engine;
@@ -72,6 +74,8 @@ int main()
 	TileLayer layer2(&shader2);
 	layer2.add(new Sprite(-2, -2, 4, 4, maths::vec4(1, 0, 1, 1)));
 
+	Texture texture("test.png");
+
 	Timer time;
 	float timer = 0;
 	unsigned int frames = 0;
@@ -103,3 +107,51 @@ int main()
 	
 	return 0;
 }
+#endif
+
+#if 0
+int main()
+{
+	const char* filename = "test.png";
+
+	//image format
+	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+	//pointer to the image, once loaded
+	FIBITMAP *dib(0);
+	//pointer to the image data
+	BYTE* bits(0);
+	//image width and height
+	unsigned int width(0), height(0);
+	//OpenGL's image ID to map to
+	GLuint gl_texID;
+
+	//check the file signature and deduce its format
+	fif = FreeImage_GetFileType(filename, 0);
+	//if still unknown, try to guess the file format from the file extension
+	if (fif == FIF_UNKNOWN)
+		fif = FreeImage_GetFIFFromFilename(filename);
+	//if still unkown, return failure
+	if (fif == FIF_UNKNOWN)
+		return false;
+
+	//check that the plugin has reading capabilities and load the file
+	if (FreeImage_FIFSupportsReading(fif))
+		dib = FreeImage_Load(fif, filename);
+	//if the image failed to load, return failure
+	if (!dib)
+		return false;
+
+	//retrieve the image data
+	bits = FreeImage_GetBits(dib);
+	//get the image width and height
+	width = FreeImage_GetWidth(dib);
+	height = FreeImage_GetHeight(dib);
+	//if this somehow one of these failed (they shouldn't), return failure
+	if ((bits == 0) || (width == 0) || (height == 0))
+		return false;
+
+	std::cout << width << ", " << height << std::endl;
+
+	return 0;
+}
+#endif
